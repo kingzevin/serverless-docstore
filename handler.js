bc37a6ecaeb3c1f6a6e320abafb5e687a4d52294
King.Zevin <king.zevin@gmail.com>
2020-10-23 10:52:28 +0800

    v3.1 - added handler.js

--- handler.js
+++ handler.js
@@ -1,11 +1,11 @@
 'use strict'
 // environment variables specified by the user
-process.env["MONGO_CONNECTION_STRING"] = `mongodb://172.17.0.1:27017/sharelatex`; // docstore.config.url
+process.env["MONGO_CONNECTION_STRING"] = `mongodb://172.17.0.1:27017/sharelatex`; // docstore.config.envs
 // the user should specify the express listener
-const expressListener = require('./app.js') // docstore.express.file
+const expressListener = require('./app.js') // docstore.express.object
 
 const owServerlessExpress = require('./owServerlessExpress.js')
 
-exports.main = function(params){ // docstore.handler.function
+exports.main = function(params){ 
   return owServerlessExpress(expressListener, params)
 }
